8ff8bbeb5ed521d7f6255d592fc3495328e14489
Jesus Laucho <jlaucho@gmail.com>
2018-07-25 06:57:50 -0400

    Agregando validaciones a el registro de usuarios

--- src/app/pages/user/register/user-register.component.ts
+++ src/app/pages/user/register/user-register.component.ts
@@ -14,26 +14,46 @@ export class UserRegisterComponent implements OnInit {
 
   forma: FormGroup;
   user: User;
-  unamePattern = '^[a-z0-9_-]{6,15}$';
+  unamePattern = '^[a-zA-Z ]+$';
+  ucedulaPattern = '^[0-9]+$';
+  caracterMin = 4;
+  caracterMax = 25;
 
-  constructor( private _userService: UserService,
+ 
+
+   constructor( private _userService: UserService,
                private _validatorsService: ValidatorsService ) { }
 
   ngOnInit() {
     this.forma = new FormGroup({
-      name: new FormControl(null, [Validators.minLength(2),
-                          Validators.maxLength(40),
+      name: new FormControl(null, [Validators.minLength( this.caracterMin ),
+                          Validators.maxLength( this.caracterMax ),
                           Validators.required,
                           Validators.pattern(this.unamePattern)]),
-      apellido: new FormControl('laucho', [Validators.minLength(2), Validators.maxLength(40), Validators.required]),
-      cedula: new FormControl('14136448', [Validators.minLength(2), Validators.maxLength(40), Validators.required]),
-      direccion: new FormControl('San rafael'),
+      apellido: new FormControl('laucho', [Validators.minLength(this.caracterMin),
+                          Validators.maxLength(this.caracterMax),
+                          Validators.required,
+                          Validators.pattern(this.unamePattern)]),
+      cedula: new FormControl('14136448', [Validators.minLength(7),
+                          Validators.maxLength(8),
+                          Validators.required,
+                          Validators.pattern(this.ucedulaPattern),
+                          Validators.min(1000000)]),
+      direccion: new FormControl(null, [Validators.required,
+                          Validators.minLength(6),
+                          Validators.maxLength(250)]),
       telefono:  new FormControl('04165608003', [Validators.required]),
-      email: new FormControl(null, Validators.required, this.emailTaken.bind(this)),
-      type: new FormControl('superAdmin'),
-      password: new FormControl('14460484'),
-      password_confirmation: new FormControl('14460484'),
-    });
+      email: new FormControl(null, [Validators.required,
+                          Validators.email]),
+      type: new FormControl('superAdmin', [Validators.required]),
+      password: new FormControl(null, [Validators.required,
+                          Validators.minLength(6),
+                          Validators.maxLength(20)]),
+      password_confirmation: new FormControl(null, [Validators.required,
+                          Validators.minLength(6),
+                          Validators.maxLength(20)]),
+    }, { validators: this.sonIguales()});
+    // Se quita el autorrellenado de las casillas
     let entradas = window.document.getElementById('form-register').getElementsByTagName('input');
     for (const key in entradas) {
       if (entradas.hasOwnProperty(key)) {
@@ -42,9 +62,22 @@ export class UserRegisterComponent implements OnInit {
       }
     }
   }
+
+  sonIguales () {
+    return  (group: FormGroup) => {
+      let pass1 = group.controls['password'].value;
+      let pass2 = group.controls['password_confirmation'].value;
+
+      if ( pass1 === pass2 ) {
+        return null;
+      }
+      return {Iguales: true};
+    };
+  }
+
   enviarFormulario() {
     this.user = this.forma.value;
-    console.log( this.forma.controls['name'] );
+    console.log( this.forma.errors );
     // this._userService.registerUser( this.user )
     //   .subscribe( (respuesta: any ) => {
     //     console.log( respuesta );
@@ -55,9 +88,12 @@ export class UserRegisterComponent implements OnInit {
   emailTaken(  ) {
     let email = 'jlaucho@gmail.com';
     // console.log( email, 'Correo enviado' );
-    this._validatorsService.emailTaken( email )
+    return this._validatorsService.emailTaken( email )
         .subscribe( (respuesta: any) => {
-          return true;
+          if (respuesta) {
+            return { existe: true };
+          }
+          return null;
         });
   }
 }
