42e9e349d128566feed0a713a5faf28a13bebb1e
Jesus Laucho <jlaucho@gmail.com>
2019-01-09 11:37:47 -0400

    add basePath heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 
 export const environment = {
   production: false,
-  basePath: 'http://localhost:8000'
+  // basePath: 'http://localhost:8000'
   // basePath: 'http://localhost/personales/RB_Servicios/rb_back_laravel/public/index.php'
+  basePath: 'https://rbservicios-back-laravel.herokuapp.com'
 };
