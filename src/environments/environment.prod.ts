6c5df6d4b257cab32e205f06b9e077f4ba36101f
Jesus Laucho <jlaucho@gmail.com>
2019-01-09 11:49:06 -0400

    Add enviroment.prod.ts, for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  // basePath: 'http://localhost:8000'
+  // basePath: 'http://localhost/personales/RB_Servicios/rb_back_laravel/public/index.php'
+  basePath: 'https://rbservicios-back-laravel.herokuapp.com'
 };
