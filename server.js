54c96b53ae833e8e6f639c978af33250bacfbda1
Jesus Laucho <jlaucho@gmail.com>
2019-01-09 16:03:01 -0400

    sdfghj

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const app = expres();
+const app = express();
 
 app.use(express.static(__dirname + '/dist/rb-front-angular'));
 
