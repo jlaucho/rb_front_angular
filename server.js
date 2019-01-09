9072271d63fce9d7b38abfa62358b4e47bef3b07
Jesus Laucho <jlaucho@gmail.com>
2019-01-09 16:10:44 -0400

    Ajustando la ruta del server

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/rb-front-angular'));
+app.use(express.static(__dirname + '/dist'));
 
 //PathLocationStradegy
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/rb-front-angular/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, () => {
