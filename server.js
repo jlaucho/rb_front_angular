8ae3f612228347bb96f05f1ac0319b19030dec39
Jesus Laucho <jlaucho@gmail.com>
2019-01-09 15:43:22 -0400

    Para arreglar

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
-
-const app = expres();
 const path = require('path');
 
-app.us(express.static(__dirname + '/dist'));
+const app = expres();
 
-app.listen(process.env.PORT || 8080);
-
+app.use(express.static(__dirname + '/dist/rb-front-angular'));
 
 //PathLocationStradegy
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/rb-front-angular/index.html'));
 });
 
-console.log('Console Listening');
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Console Listening');
+});
