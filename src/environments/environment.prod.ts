86822fa6b3129fff9603f9637df084cfff8f7506
Jesus Laucho <jlaucho@gmail.com>
2019-02-13 09:45:33 -0400

    sdfgh

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // basePath: 'http://localhost:8000'
   // basePath: 'http://localhost/personales/RB_Servicios/rb_back_laravel/public/index.php'
-  basePath: 'http://rbservicios-back-laravel.herokuapp.com'
+  basePath: 'https://rbservicios-back-laravel.herokuapp.com'
 };
